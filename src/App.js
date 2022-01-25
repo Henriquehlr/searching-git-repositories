7a160bddf6f99f2b3843ce43eb15b3193e388fc5
Henriquehlr <you@example.com>
2022-01-25 14:12:14 -0300

    Todas Alterações

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Routers from './routes';
+import GlobalStyle from './styles/global';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Routers />
+    </>
+  );
+}
+
+        export default App;
